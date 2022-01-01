7ea8f89570c05032cf7b56f19deeea0b5d415ded
Mikkel Laursen <mlaursen03@gmail.com>
2021-12-31 20:45:06 -0700

    feat: Enable react strict mode for development

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // Set to `true` after v4.0.1 release since there are some issues with Concurrent Rendering
-  reactStrictMode: false,
+  reactStrictMode: true,
 };
